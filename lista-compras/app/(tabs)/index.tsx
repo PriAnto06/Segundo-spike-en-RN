c5339d9a4fa19820b535b4246c0017ca3e46e133
PriAnto06 <priscilaantonella.d.07@gmail.com>
2026-04-23 18:40:16 -0300

    hecho

--- lista-compras/app/(tabs)/index.tsx
+++ lista-compras/app/(tabs)/index.tsx
@@ -1,38 +1,23 @@
-import { useEffect, useState } from "react";
-import { View, StyleSheet } from "react-native";
 import Contenedor from "../../components/Contenedor";
 import TituloDeLaPagina from "../../components/TituloDeLaPagina";
 import FormularioParaItemNuevo from "../../components/FormularioParaItemNuevo";
 import ListaDeCompras from "../../components/ListaDeCompras";
-import { Item } from "../../tipos/item";
 import useItemsDeCompra from "@/hooks/usarItemsDeCompra";
 
-
 export default function App() {
-
-
   const { items, agregarItem, cambiarItem, eliminarItem } = useItemsDeCompra();
 
   return (
     <Contenedor>
       <TituloDeLaPagina />
 
-      <View style={styles.listaContainer}>
-        <ListaDeCompras
-          items={items}
-          alPresionarSobreUnItem={cambiarItem}
-          alMantenerPresionSobreUnItem={eliminarItem}
-        />
-      </View>
+      <ListaDeCompras
+        items={items}
+        alPresionarSobreUnItem={cambiarItem}
+        alMantenerPresionSobreUnItem={eliminarItem}
+      />
 
       <FormularioParaItemNuevo alCompletarFormulario={agregarItem} />
     </Contenedor>
   );
 }
-
-const styles = StyleSheet.create({
-  listaContainer: {
-    flex: 1,
-    minHeight: 200, // asegura que haya espacio aunque la lista esté vacía
-  },
-});
--- lista-compras/components/ListaDeCompras.tsx
+++ lista-compras/components/ListaDeCompras.tsx
@@ -11,43 +11,42 @@ export default function ListaDeCompras({
   alPresionarSobreUnItem: (id: string) => void;
   alMantenerPresionSobreUnItem: (id: string) => void;
 }) {
-
   useEffect(() => {
-  console.log("items actualizado:", items);
-}, [items]);
-  
+    console.log("items actualizado:", items);
+  }, [items]);
+
   return (
-    <FlatList
-      style={{ flex: 1 }}
-      contentContainerStyle={{ flexGrow: 1 }}
-      data={items}
-      keyExtractor={(it) => it.id}
-      renderItem={({ item }) => (
-        <Pressable
-          onPress={() => alPresionarSobreUnItem(item.id)}
-          onLongPress={() => alMantenerPresionSobreUnItem(item.id)}
-          style={styles.row}
-        >
-          <Text style={[styles.rowText, item.done && styles.done]}>
-            {item.name}
-          </Text>
-          <Text
-            style={[
-              styles.pill,
-              item.done ? styles.pillDone : styles.pillTodo,
-            ]}
+    <View styles={styles.listaContainer}>
+      <FlatList
+        style={{ flex: 1 }}
+        contentContainerStyle={{ flexGrow: 1 }}
+        data={items}
+        keyExtractor={(it) => it.id}
+        renderItem={({ item }) => (
+          <Pressable
+            onPress={() => alPresionarSobreUnItem(item.id)}
+            onLongPress={() => alMantenerPresionSobreUnItem(item.id)}
+            style={styles.row}
           >
-            {item.done ? "✔" : "•"}
-          </Text>
-        </Pressable>
-      )}
-      ListEmptyComponent={
-        <Text style={styles.empty}>
-          Sin productos. ¡Agregá el primero!
-        </Text>
-      }
-      ItemSeparatorComponent={() => <View style={styles.sep} />}
-    />
+            <Text style={[styles.rowText, item.done && styles.done]}>
+              {item.name}
+            </Text>
+            <Text
+              style={[
+                styles.pill,
+                item.done ? styles.pillDone : styles.pillTodo,
+              ]}
+            >
+              {item.done ? "✔" : "•"}
+            </Text>
+          </Pressable>
+        )}
+        ListEmptyComponent={
+          <Text style={styles.empty}>Sin productos. ¡Agregá el primero!</Text>
+        }
+        ItemSeparatorComponent={() => <View style={styles.sep} />}
+      />
+    </View>
   );
 }
 
@@ -60,4 +59,8 @@ const styles = StyleSheet.create({
   pillDone: { backgroundColor: "#2ecc71", color: "#fff" },
   sep: { height: 1, backgroundColor: "#eee" },
   empty: { textAlign: "center", marginTop: 20 },
+  listaContainer: {
+    flex: 1,
+    minHeight: 200, // asegura que haya espacio aunque la lista esté vacía
+  },
 });
